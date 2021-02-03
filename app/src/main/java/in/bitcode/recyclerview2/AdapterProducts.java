bc62b1aa7ffa97f52f722f867f7b456b7e3b3af8
vishaljagtap3 <vishal.jagtap@bitcode.in>
2021-02-03 15:57:57 +0530

    RecyclerView2 - updates

--- app/src/main/java/in/bitcode/recyclerview2/AdapterProducts.java
+++ app/src/main/java/in/bitcode/recyclerview2/AdapterProducts.java
@@ -1,10 +1,15 @@
 package in.bitcode.recyclerview2;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -14,9 +19,20 @@ import androidx.recyclerview.widget.RecyclerView;
 public class AdapterProducts extends RecyclerView.Adapter<AdapterProducts.ProductViewHolder> {
 
     private ArrayList<Product> mListProducts;
-
     public AdapterProducts(ArrayList<Product> listProducts) {
         this.mListProducts = listProducts;
+    }
+
+
+    public interface  OnProductClickListener {
+        public void onProductImageClick(AdapterProducts adapterProducts, Product product, int position, ImageView imgProduct);
+        public void onProductTitleClick(AdapterProducts adapterProducts, Product product, int position, TextView txtProductTitle);
+    }
+
+    private OnProductClickListener mOnProductClickListener;
+
+    public void setOnProductClickListener(OnProductClickListener onProductClickListener) {
+        this.mOnProductClickListener = onProductClickListener;
     }
 
     @NonNull
@@ -37,6 +53,22 @@ public class AdapterProducts extends RecyclerView.Adapter<AdapterProducts.Produc
         holder.mImgProduct.setImageResource(product.getImageId());
         holder.mTxtProductName.setText(product.getTitle());
 
+        //Not a good place to attach listeners
+        /*
+        holder.mImgProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mt(view.getContext(), "Image " + position  +" Clicked");
+            }
+        });
+        holder.mTxtProductName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mt(view.getContext(), mListProducts.get(position).getTitle() + " " + position  +" Clicked");
+            }
+        });
+         */
+
     }
 
     @Override
@@ -54,6 +86,67 @@ public class AdapterProducts extends RecyclerView.Adapter<AdapterProducts.Produc
 
             mImgProduct = itemView.findViewById(R.id.imgProduct);
             mTxtProductName = itemView.findViewById(R.id.txtProductName);
+
+            mImgProduct.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(mOnProductClickListener != null) {
+                        mOnProductClickListener.onProductImageClick(
+                                AdapterProducts.this,
+                                mListProducts.get(getAdapterPosition()),
+                                getAdapterPosition(),
+                                mImgProduct
+                        );
+                    }
+
+                    /*
+                    mt(view.getContext(), "Image " + getAdapterPosition() + " Clicked");
+                    //mt(view.getContext(), "Image " + " Clicked");
+                    */
+
+                }
+            });
+            mTxtProductName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(mOnProductClickListener != null) {
+                        mOnProductClickListener.onProductTitleClick(
+                                AdapterProducts.this,
+                                mListProducts.get(getAdapterPosition()),
+                                getAdapterPosition(),
+                                mTxtProductName
+                        );
+                    }
+
+                    /*
+                    mt(view.getContext(), mListProducts.get(getAdapterPosition()).getTitle() + " " + getAdapterPosition() + " Clicked");
+                    //mt(view.getContext(), "Product Name clicked");
+                    ms(view, mListProducts.get(getAdapterPosition()).getTitle() + " " + getAdapterPosition() + " Clicked");
+                    */
+
+                }
+            });
+
         }
+    }
+
+    private void ms(View view, String text) {
+        Snackbar snackbar = Snackbar.make(view, text, Snackbar.LENGTH_LONG);
+        snackbar.setAction("Dismiss", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.setActionTextColor(Color.YELLOW);
+        snackbar.setTextColor(Color.RED);
+        snackbar.show();
+    }
+
+    private void mt(Context context, String text) {
+        //Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+
     }
 }
--- app/src/main/java/in/bitcode/recyclerview2/MainActivity.java
+++ app/src/main/java/in/bitcode/recyclerview2/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -23,12 +27,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        mListProducts = new ArrayList<>();
-        mListProducts.add(new Product(1, "Laptop", R.mipmap.ic_launcher, 1234.34f));
-        mListProducts.add(new Product(2, "Projector", R.mipmap.ic_launcher, 1234.34f));
-        mListProducts.add(new Product(3, "Camera", R.mipmap.ic_launcher, 1234.34f));
-        mListProducts.add(new Product(4, "Phone", R.mipmap.ic_launcher, 1234.34f));
+        {
+            mListProducts = new ArrayList<>();
+            mListProducts.add(new Product(1, "1Laptop", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(2, "2Projector", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(3, "3Camera", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "4Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "5Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "6Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "7Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "8Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "9Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "10Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "11Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "12Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "13Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "14Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "15Phone", R.mipmap.ic_launcher, 1234.34f));
+            mListProducts.add(new Product(4, "16Phone", R.mipmap.ic_launcher, 1234.34f));
 
+        }
         mRecyclerProducts = findViewById(R.id.recyclerProducts);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerProducts.setLayoutManager(linearLayoutManager);
@@ -36,6 +54,27 @@ public class MainActivity extends AppCompatActivity {
         mAdapterProducts = new AdapterProducts(mListProducts);
         mRecyclerProducts.setAdapter(mAdapterProducts);
 
+        mAdapterProducts.setOnProductClickListener(new MyOnProductClickListener());
 
+    }
+
+    private class MyOnProductClickListener implements AdapterProducts.OnProductClickListener {
+        @Override
+        public void onProductImageClick(AdapterProducts adapterProducts, Product product, int position, ImageView imgProduct) {
+            Snackbar.make(
+                    mRecyclerProducts,
+                    product.getTitle(),
+                    Snackbar.LENGTH_LONG
+            ).show();
+        }
+
+        @Override
+        public void onProductTitleClick(AdapterProducts adapterProducts, Product product, int position, TextView txtProductTitle) {
+            Snackbar.make(
+                    mRecyclerProducts,
+                    product.getTitle() + " " + position,
+                    Snackbar.LENGTH_LONG
+            ).show();
+        }
     }
 }
